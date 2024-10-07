3bbc5294a8a779d6d0f0eb86c982a521cd67fb81
SHANI YADAV <yshani477@gmail.com>
2024-10-07 12:28:03 +0530

    Adding a index.js file into my github repo

---
+++ index.js
@@ -0,0 +1,11 @@
+const number =[2, 3, 5 ,4 ,34 ,56 ,23,53, 75,56,457,235,67];
+for(let i=0;i<number.length;i++)
+{
+    if(number[i]%2==0)
+    {
+        console.log("This is a even number",number[i]);
+    }
+    else {
+        console.log("This is a odd number",number[i]);
+    }
+}
